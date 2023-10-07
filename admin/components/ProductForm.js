2be3ec4a502b953323a45b6278d511fa35cb7bf0
MrV09 <vladcirlugea@gmail.com>
2023-10-07 17:24:35 +0300

    Finished Products and Categories tabs for Admin

--- admin/components/ProductForm.js
+++ admin/components/ProductForm.js
@@ -1,14 +1,22 @@
 import { useRouter } from "next/router";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
-export default function ProductForm({_id,name:existingName,description:existingDescription,price:existingPrice,images:existingImages,}){
+export default function ProductForm({_id,name:existingName,description:existingDescription,price:existingPrice,images:existingImages,category:existingCategory,}){
     const [name, setName] = useState(existingName || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
     const [images, setImages] = useState(existingImages || []);
+    const [category, setCategory] = useState(existingCategory || '');
     const [goTOProducts, setGoToProducts] = useState(false);
+    const [categories, setCategories] = useState([]);
     const router = useRouter();
+
+    useEffect(() => {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data); 
+        })
+    }, []);
 
     async function saveProduct(ev){
         ev.preventDefault();
@@ -17,6 +25,7 @@ export default function ProductForm({_id,name:existingName,description:existingD
             description,
             price,
             images,
+            category,
         };
         if(_id){     
             await axios.put('/api/products', {...product, _id});
@@ -42,12 +51,37 @@ export default function ProductForm({_id,name:existingName,description:existingD
         }
     }
 
+    const allProperties = [];
+    if(categories.length > 0 && category){
+        let categoryInfo = categories.find(({_id}) => _id === category);
+        allProperties.push(...categoryInfo.properties);
+        while(categoryInfo?.parent?._id){
+            const parentCat = categories.find(({_id}) => _id === categoryInfo?.parent?._id);
+            allProperties.push(...parentCat.properties);
+            categoryInfo = parentCat;
+        }
+    }
+
     return (
         <form onSubmit={saveProduct}>
             <label>Product Name</label>
             <input type="text" placeholder="Product name" value={name} onChange={ev => setName(ev.target.value)}/>
+            <label>Product Category</label>
+            <select value={category} onChange={ev => setCategory(ev.target.value)}>
+                <option value="">No Category</option>
+                {categories.length > 0 && categories.map(category => (
+                    <option value={category._id}>{category.name}</option>
+                ))}
+            </select>
+            {allProperties.length > 0 && allProperties.map(property => (
+                <div className="flex gap-2">
+                    <div>
+                        {property.name}
+                    </div>
+                </div>
+            ))}
             <label>Images</label>
-            <div className="mb-2 flex-wrap gap-2 py-2">
+            <div className="mb-2 flex gap-2 py-2">
                 {!!images?.length && images.map(link =>(
                     <div key={link} className="h-20 rounded-lg">
                         <img src={link} />
@@ -63,7 +97,7 @@ export default function ProductForm({_id,name:existingName,description:existingD
             </div>
             <label>Product Description</label>
             <textarea placeholder="Description" value={description} onChange={ev => setDescription(ev.target.value)} ></textarea>
-            <label>Product Price (in EUR)</label>
+            <label>Product Price (in RON)</label>
             <input type="number" placeholder="Price" value={price} onChange={ev => setPrice(ev.target.value)} />
             <button type="submit" className="btn_primary">Save Product</button>
         </form>
--- admin/pages/categories.js
+++ admin/pages/categories.js
@@ -9,6 +9,7 @@ const [editedCategory, setEditedCategory] = useState(null);
 const [name, setName] = useState("");
 const [parentCategory, setParentCategory] = useState("");
 const [categories, setCategories] = useState([]);
+const [properties, setProperties] = useState([]);
 
 useEffect(() => {
     fetchCategories();
@@ -21,8 +22,8 @@ function fetchCategories(){
 }
 
 async function saveCategory(ev){
-    const data = {name, parentCategory};
     ev.preventDefault();
+    const data = {name, parentCategory,properties:properties.map(property => ({name:property.name,values:property.values.split(',')})),};
     if(editedCategory){
         data._id = editedCategory._id;
         await axios.put('/api/categories', data);
@@ -31,6 +32,8 @@ async function saveCategory(ev){
         await axios.post('/api/categories', data);
     }
     setName("");
+    setParentCategory("");
+    setProperties([]);
     fetchCategories();
 }
 
@@ -38,6 +41,7 @@ function editCategory(category){
     setEditedCategory(category);
     setName(category.name);
     setParentCategory(category.parent?._id);
+    setProperties(category.properties);
 }
 
 function deleteCategory(category){
@@ -48,10 +52,42 @@ function deleteCategory(category){
         cancelButtonText: 'Cancel!',
         confirmButtonText: 'Confirm!',
         confirmButtonColor: '#0B5DCD'
-    }).then(result => {
+    }).then(async result => {
+        if(result.isConfirmed){
+            const {_id} = category;
+            await axios.delete('/api/categories?_id='+_id);
+            fetchCategories();
+        }
+    });
+}
 
-    }).catch(error => {
+function addProperty(){
+    setProperties(old => {
+        return [...old, {name: '', values: ''}];
+    });
+}
 
+function updatePropertyName(index, property, newName){
+    setProperties(old => {
+        const properties = [...old];
+        properties[index].name = newName;
+        return properties;
+    });
+}
+
+function updatePropertyValues(index, property, newValues){
+    setProperties(old => {
+        const properties = [...old];
+        properties[index].values = newValues;
+        return properties;
+    });
+}
+
+function removeProperty(indexToRemove){
+    setProperties(old => {
+        return [...old].filter((p,index) => {
+            return index !== indexToRemove;
+        });
     });
 }
 
@@ -59,9 +95,10 @@ function deleteCategory(category){
         <Layout>
             <h1>Categories</h1>
             <label>{editedCategory ? `Edit Category: ${editedCategory.name}` : 'New Category Name'}</label>
-            <form onSubmit={saveCategory} className="flex gap-2">
-                <input className="mb-0 h-16" type="text" placeholder={"Category name"} value={name} onChange={ev => setName(ev.target.value)}/>
-                <select className="mb-0" value={parentCategory} onChange={ev => setParentCategory(ev.target.value)}>
+            <form onSubmit={saveCategory}>
+                <div className="flex gap-1">
+                <input className="h-16" type="text" placeholder={"Category name"} value={name} onChange={ev => setName(ev.target.value)}/>
+                <select className="" value={parentCategory} onChange={ev => setParentCategory(ev.target.value)}>
                     <option value="">No parent category</option>
                     {categories.length > 0 && categories.map(category => (
                         <option value={category._id}>
@@ -69,9 +106,27 @@ function deleteCategory(category){
                         </option>
                     ))}
                 </select>
-                <button type="submit" className="btn_primary h-16 py-1">Add Category!</button>
+                </div>
+                <div className="mb-4">
+                    <label className="block">Properties</label>
+                    <button onClick={addProperty} type="button" className="btn_primary text-sm mb-2">Add new property</button>
+                    {properties.length > 0 && properties.map((property, index) => (
+                        <div className="flex gap-1 mb-2">
+                            <input className="mb-0" type="text" value={property.name} onChange={ev => updatePropertyName(index, property, ev.target.value)} placeholder="Property name"/>
+                            <input className="mb-0" type="text" value={property.values} onChange={ev => updatePropertyValues(index, property, ev.target.value)} placeholder="Values, separated by ','"/>
+                            <button type="button" onClick={() => removeProperty(index)} className="btn_primary">Remove</button>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex gap-2 h-12">
+                {editedCategory && (
+                    <button type="button" onClick={() => {setEditedCategory(null); setName(''); setParentCategory(''); setProperties([]);}} className="btn_primary">Cancel</button>
+                )}
+                <button type="submit" className="btn_primary h-12 py-1">Add Category!</button>
+                </div>
             </form>
-            <table className="basic mt-4">
+            {!editedCategory && (
+                <table className="basic mt-4">
                 <thead>
                     <tr>
                         <td className="font-bold">Category name</td>
@@ -94,6 +149,7 @@ function deleteCategory(category){
                     ))}
                 </tbody>
             </table>
+            )}         
         </Layout>
     );
 }
